a4eb74e8c6fdbe50b0095790fa8d3b84b5f32ab9
wentaojiang <wentao.jiang@shuyun.com>
2019-09-01 11:38:23 +0800

    add question16

---
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question16.java
@@ -0,0 +1,33 @@
+package com.wentjiang.leetcode.q1_100;
+
+import java.util.Arrays;
+
+/**
+ * @author wentaojiang
+ * @date 2019/8/30 8:32 AM
+ * @description
+ */
+public class Question16 {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (Math.abs(target - sum) < Math.abs(target - result)) {
+                    result = sum;
+                }
+                if (target > sum) {
+                    start++;
+                } else if (sum > target) {
+                    end--;
+                } else {
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q1_100/Question16Test.java
@@ -0,0 +1,36 @@
+package com.wentjiang.leetcode.q1_100;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wentaojiang
+ * @date 2019/8/30 8:34 AM
+ * @description
+ */
+public class Question16Test {
+    private Question16 question16;
+
+    @Before
+    public void setUp() throws Exception {
+        question16 = new Question16();
+    }
+
+    @Test
+    public void threeSumClosest1() {
+        Assert.assertEquals(2, question16.threeSumClosest(new int[]{-1, 2, 1, -4}, 1));
+    }
+
+    @Test
+    public void threeSumClosest2() {
+        Assert.assertEquals(0, question16.threeSumClosest(new int[]{0, 2, 1, -3}, 0));
+    }
+
+    @Test
+    public void threeSumClosest3() {
+        Assert.assertEquals(0, question16.threeSumClosest(new int[]{0, 2, 1, -3}, 1));
+    }
+}
