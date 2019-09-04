a0b0b4e11f0a351173b3510e928fa6478af29bcd
wentaojiang <wentao.jiang@shuyun.com>
2019-09-04 18:28:50 +0800

    add question6

--- src/main/java/com/wentjiang/leetcode/q1_100/Question6.java
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question6.java
@@ -1,5 +1,10 @@
 package com.wentjiang.leetcode.q1_100;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 /**
  * @author wentaojiang
  * @date 2019/9/1 9:23 PM
@@ -7,7 +12,33 @@ package com.wentjiang.leetcode.q1_100;
  */
 public class Question6 {
     public String convert(String s, int numRows) {
-
-        return "";
+        if (numRows == 1) {
+            return s;
+        }
+        List<List<Character>> result = new ArrayList<>(numRows);
+        for (int i = 0; i < numRows; i++) {
+            result.add(new ArrayList<>());
+        }
+        boolean down = true;
+        int currentLine = 0;
+        char[] chars = s.toCharArray();
+        for (char aChar : chars) {
+            if (currentLine == 0) {
+                down = true;
+            }
+            if (currentLine == numRows - 1) {
+                down = false;
+            }
+            result.get(currentLine).add(aChar);
+            if (down) {
+                currentLine++;
+            } else {
+                currentLine--;
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        result.stream().flatMap((Function<List<Character>, Stream<?>>) characters -> characters.stream())
+            .forEach(stringBuilder::append);
+        return stringBuilder.toString();
     }
 }
---
+++ src/test/java/com/wentjiang/leetcode/q1_100/Question6Test.java
@@ -0,0 +1,32 @@
+package com.wentjiang.leetcode.q1_100;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wentaojiang
+ * @date 2019/9/4 5:37 PM
+ * @description
+ */
+public class Question6Test {
+
+    private Question6 question6;
+
+    @Before
+    public void setUp() throws Exception {
+        question6 = new Question6();
+    }
+
+    @Test
+    public void convert1() {
+        Assert.assertEquals("LCIRETOESIIGEDHN", question6.convert("LEETCODEISHIRING", 3));
+    }
+
+    @Test
+    public void convert2() {
+        Assert.assertEquals("LDREOEIIECIHNTSG", question6.convert("LEETCODEISHIRING", 4));
+    }
+}
