07b50ec78cff44f9dbb4987c30543d78046d4861
wentjiang <wentjiang@sina.com>
2019-09-07 09:14:27 +0800

    add question17

---
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question17.java
@@ -0,0 +1,40 @@
+package com.wentjiang.leetcode.q1_100;
+
+import java.util.*;
+
+/**
+ * @author wentaojiang
+ * @date 2019/9/7 8:52 AM
+ * @description
+ */
+public class Question17 {
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        map.put("2", Arrays.asList("a", "b", "c"));
+        map.put("3", Arrays.asList("d", "e", "f"));
+        map.put("4", Arrays.asList("g", "h", "i"));
+        map.put("5", Arrays.asList("j", "k", "l"));
+        map.put("6", Arrays.asList("m", "n", "o"));
+        map.put("7", Arrays.asList("p", "q", "r", "s"));
+        map.put("8", Arrays.asList("t", "u", "v"));
+        map.put("9", Arrays.asList("w", "x", "y", "z"));
+        for (char ch : digits.toCharArray()) {
+            result = getResult(result, map.get(String.valueOf(ch)));
+        }
+        return result;
+    }
+
+    public List<String> getResult(List<String> current, List<String> strs) {
+        if (current.size() == 0) {
+            return strs;
+        }
+        List<String> result = new ArrayList<>();
+        for (String str : current) {
+            for (String ch : strs) {
+                result.add(str + ch);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q1_100/Question17Test.java
@@ -0,0 +1,25 @@
+package com.wentjiang.leetcode.q1_100;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wentaojiang
+ * @date 2019/9/7 8:53 AM
+ * @description
+ */
+public class Question17Test {
+
+    private Question17 question17;
+
+    @Before
+    public void setUp() throws Exception {
+        question17 = new Question17();
+    }
+
+    @Test
+    public void letterCombinations() {
+    }
+}
