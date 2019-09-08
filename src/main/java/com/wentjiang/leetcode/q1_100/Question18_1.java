de3ae053882134e9bcc16cc1fe4333727d5caed6
wentjiang <wentjiang@sina.com>
2019-09-08 21:07:56 +0800

    optimize 18

--- src/main/java/com/wentjiang/leetcode/q1_100/Question18_1.java
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question18_1.java
@@ -5,7 +5,7 @@ import java.util.*;
 /**
  * @author wentaojiang
  * @date 2019/9/8 10:00 AM
- * @description 有性能问题, 提交之后超时
+ * @description 时间还是比较慢
  */
 public class Question18_1 {
     public List<List<Integer>> fourSum(int[] nums, int target) {
@@ -14,6 +14,7 @@ public class Question18_1 {
             return result;
         }
         Arrays.sort(nums);
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < nums.length - 3; i++) {
             for (int j = i + 1; j < nums.length - 2; j++) {
                 int start = j + 1;
@@ -21,27 +22,20 @@ public class Question18_1 {
                 while (start < end) {
                     int sum = nums[i] + nums[j] + nums[start] + nums[end];
                     if (sum == target) {
-                        List<Integer> list = Arrays.asList(nums[i], nums[j], nums[start], nums[end]);
-                        result.add(list);
-                        start++;
+                        String key = nums[i] + "-" + nums[j] + "-" + nums[start] + "-" + nums[end];
+                        if (set.contains(key)) {
+                            start++;
+                        } else {
+                            set.add(key);
+                            List<Integer> list = Arrays.asList(nums[i], nums[j], nums[start], nums[end]);
+                            result.add(list);
+                            start++;
+                        }
                     } else if (sum < target) {
                         start++;
                     } else {
                         end--;
                     }
-                }
-            }
-            Iterator<List<Integer>> iterator = result.iterator();
-            Set<String> set = new HashSet<>();
-            while (iterator.hasNext()) {
-                List<Integer> list = iterator.next();
-                StringBuilder stringBuilder = new StringBuilder();
-                list.forEach(stringBuilder::append);
-                String string = stringBuilder.toString();
-                if (set.contains(string)) {
-                    iterator.remove();
-                } else {
-                    set.add(string);
                 }
             }
         }
