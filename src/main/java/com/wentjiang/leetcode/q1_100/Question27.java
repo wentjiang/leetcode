59b196c35f428c5754987d330765ec7281c097d4
wentjiang <wentjiang@sina.com>
2019-09-10 15:20:31 +0800

    add question27

---
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question27.java
@@ -0,0 +1,19 @@
+package com.wentjiang.leetcode.q1_100;
+
+/**
+ * @author wentaojiang
+ * @date 2019/9/10 2:59 PM
+ * @description
+ */
+public class Question27 {
+    public int removeElement(int[] nums, int val) {
+        int deleteNum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i - deleteNum] = nums[i];
+            if (nums[i] == val) {
+                deleteNum++;
+            }
+        }
+        return nums.length - deleteNum;
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q1_100/Question27Test.java
@@ -0,0 +1,33 @@
+package com.wentjiang.leetcode.q1_100;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wentaojiang
+ * @date 2019/9/10 3:04 PM
+ * @description
+ */
+public class Question27Test {
+
+    private Question27 question27;
+
+    @Before
+    public void setUp() throws Exception {
+        question27 = new Question27();
+    }
+
+    @Test
+    public void removeElement() {
+        Assert.assertEquals(2, question27.removeElement(new int[]{3, 2, 2, 3}, 3));
+    }
+
+    @Test
+    public void removeElement1() {
+        Assert.assertEquals(5, question27.removeElement(new int[]{0,1,2,2,3,0,4,2}, 2));
+    }
+
+}
