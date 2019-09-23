5d457e839d025cef5499b1bfe55dfca07c4fc394
wentjiang <wentjiang@sina.com>
2019-09-23 15:28:15 +0800

    add question83

---
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question_83.java
@@ -0,0 +1,58 @@
+package com.wentjiang.leetcode.q1_100;
+
+/**
+ * @author wentao.jiang
+ * @date 2019/9/23 1:41 PM
+ * @description
+ */
+public class Question_83 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode first = new ListNode(0);
+        first.next = head;
+        ListNode pre = head;
+        ListNode preNext = head.next;
+        while (pre.next != null && preNext != null) {
+            if (preNext.val == pre.val) {
+                pre.next = preNext.next;
+                if (preNext.next != null) {
+                    preNext = preNext.next;
+                }
+            } else {
+                pre = pre.next;
+                preNext = preNext.next;
+            }
+        }
+        return first.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode first = new ListNode(0);
+        ListNode head = first;
+
+        head.next = new ListNode(1);
+        head = head.next;
+        head.next = new ListNode(2);
+        head = head.next;
+        head.next = new ListNode(2);
+        head = head.next;
+        head.next = new ListNode(3);
+        head = head.next;
+        head.next = new ListNode(3);
+
+        ListNode node = deleteDuplicates(first.next);
+        System.out.println(node);
+    }
+}
