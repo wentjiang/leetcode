03ecdbf0577780d2a2a89ee6e5e0d8aa5ddf145f
wentjiang <wentjiang@sina.com>
2019-10-07 13:57:04 +0800

    add question_42

--- src/main/java/com/wentjiang/leetcode/q1_100/Question_42.java
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question_42.java
@@ -8,9 +8,56 @@ package com.wentjiang.leetcode.q1_100;
 public class Question_42 {
     public int trap(int[] height) {
         int sum = 0;
-        //todo
+        for (int i = 0; i < height.length; ) {
+            if (i != height.length - 1 && height[i + 1] >= height[i]) {
+                i++;
+                continue;
+            }
+            //当前高度
+            int current = height[i];
+            //后边比当前高度高的高度
+            int higher = 0;
+            //后边比当前高度高的索引
+            int higherIndex = i;
+            boolean isIHighest = true;
+            int secondHigh = 0;
+            int secondHighIndex = i;
 
+            //找下一个比当前元素高的元素
+            for (int j = i + 2; j < height.length; j++) {
+                if (height[j] >= height[i]) {
+                    higher = height[j];
+                    higherIndex = j;
+                    isIHighest = false;
+                    break;
+                } else {
+                    if (height[j] > secondHigh) {
+                        secondHigh = height[j];
+                        secondHighIndex = j;
+                    }
+                }
+            }
+            //发现更高的
+            if (higher != 0 && higherIndex != i && !isIHighest) {
+                for (int j = 1; j < higherIndex - i; j++) {
+                    sum += height[i] - height[i + j];
+                }
+                i = higherIndex;
+            } else if (isIHighest) {
+                for (int j = 1; j < secondHighIndex - i; j++) {
+                    int temp = secondHigh - height[i + j];
+                    if (temp > 0) {
+                        sum += temp;
+                    }
+                }
+                if (i == secondHighIndex) {
+                    i++;
+                } else {
+                    i = secondHighIndex;
+                }
+
+            }
+        }
         return sum;
     }
-
 }
---
+++ src/test/java/com/wentjiang/leetcode/q1_100/Question_42Test.java
@@ -0,0 +1,37 @@
+package com.wentjiang.leetcode.q1_100;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wentao.jiang
+ * @date 2019/10/7 12:13 AM
+ * @description
+ */
+public class Question_42Test {
+
+    private Question_42 question_42;
+
+    @Before
+    public void setUp() throws Exception {
+        question_42 = new Question_42();
+    }
+
+    @Test
+    public void trap() {
+        Assert.assertEquals(6, question_42.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+
+    @Test
+    public void trap1() {
+        Assert.assertEquals(1, question_42.trap(new int[]{4,2,3}));
+    }
+
+    @Test
+    public void trap2() {
+        Assert.assertEquals(1, question_42.trap(new int[]{5,4,1,2}));
+    }
+}
