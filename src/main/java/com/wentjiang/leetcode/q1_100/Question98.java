819d4773d81213526c5ab94f4acff44f912ebae1
wentjiang <wentjiang@sina.com>
2019-10-07 15:13:09 +0800

    add question98

---
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question98.java
@@ -0,0 +1,37 @@
+package com.wentjiang.leetcode.q1_100;
+
+/**
+ * @author wentao.jiang
+ * @date 2019/10/7 2:34 PM
+ * @description
+ */
+public class Question98 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+
+    public boolean helper(TreeNode node, Integer low, Integer high) {
+        if (node == null) {
+            return true;
+        }
+        int val = node.val;
+        if (low != null && val <= low) {
+            return false;
+        }
+        if (high != null && val >= high) {
+            return false;
+        }
+        return helper(node.right, val, high) && helper(node.left, low, val);
+    }
+}
