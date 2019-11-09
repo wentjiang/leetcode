ed6cd7f99c70092dfbafaa81bc38c74fdce2b62e
wentjiang <wentjiang@sina.com>
2019-11-09 12:05:31 +0800

    add question106

--- src/main/java/com/wentjiang/leetcode/q101_200/Question106.java
+++ src/main/java/com/wentjiang/leetcode/q101_200/Question106.java
@@ -21,33 +21,29 @@ public class Question106 {
 
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         int length = inorder.length;
-
+        if (length == 0) {
+            return null;
+        }
         //获取后序遍历中后边的元素第一个出现在当前中序的元素当做root,因为递归之后,后序的元素可能不在中序的数组中,这里用遍历方式解决
-        TreeNode root = null;
+        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
         int index = -1;
-        for (int j = postorder.length - 1; j >= 0; j--) {
-            for (int i = 0; i < inorder.length; i++) {
-                if (postorder[j] == inorder[i]) {
-                    index = i;
-                    root = new TreeNode(postorder[j]);
-                    break;
-                }
-            }
-            if (root != null) {
+        for (int i = 0; i < inorder.length; i++) {
+            if (root.val == inorder[i]) {
+                index = i;
                 break;
             }
         }
-        if (root == null) {
-            return null;
-        }
 
         //构建左子树
-        root.left = buildTree(Arrays.copyOfRange(inorder, 0, index)
-            , Arrays.copyOfRange(postorder, 0, postorder.length - 1));
+        if (index > 0) {
+            root.left = buildTree(Arrays.copyOfRange(inorder, 0, index)
+                , Arrays.copyOfRange(postorder, 0, index));
+        }
         //构建右子树
-        root.right = buildTree(Arrays.copyOfRange(inorder, index + 1, length)
-            , Arrays.copyOfRange(postorder, 0, postorder.length - 1));
-
+        if (index + 1 < length) {
+            root.right = buildTree(Arrays.copyOfRange(inorder, index + 1, length)
+                , Arrays.copyOfRange(postorder, index, postorder.length - 1));
+        }
 
         return root;
     }
