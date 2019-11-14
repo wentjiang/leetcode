e12deb2314be1141a8591a623180cdaf06fae8bb
wentjiang <wentjiang@sina.com>
2019-11-14 22:05:27 +0800

    add question31

---
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question31.java
@@ -0,0 +1,39 @@
+package com.wentjiang.leetcode.q1_100;
+
+/**
+ * @author wentao.jiang
+ * @date 2019/11/14 8:52 PM
+ * @description
+ */
+public class Question31 {
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reversal(nums, i + 1, nums.length - 1);
+
+    }
+
+    private void reversal(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start++, end--);
+        }
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
+
+}
