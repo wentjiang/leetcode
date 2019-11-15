fc8c2eea6a1a4d48c173d6f73fab4333ca124800
wentjiang <wentjiang@sina.com>
2019-11-15 14:42:45 +0800

    improve question56

---
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question56.java
@@ -0,0 +1,47 @@
+package com.wentjiang.leetcode.q1_100;
+
+import java.util.Arrays;
+
+/**
+ * @author wentao.jiang
+ * @date 2019/11/15 11:22 AM
+ * @description
+ */
+public class Question56 {
+
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+        //先进行排序
+        for (int i = 0; i < intervals.length; i++) {
+            int minIndex = i;
+            int min = Integer.MAX_VALUE;
+            for (int j = i; j < intervals.length; j++) {
+                if (intervals[i][0] > intervals[j][0] && min > intervals[j][0]) {
+                    min = intervals[j][0];
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int[] temp = intervals[i];
+                intervals[i] = intervals[minIndex];
+                intervals[minIndex] = temp;
+            }
+        }
+        //排序好进行替换
+        int current = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[current][1] >= intervals[i][0]) {
+                if (intervals[current][1] < intervals[i][1]) {
+                    intervals[current][1] = intervals[i][1];
+                }
+            } else {
+                current++;
+                intervals[current][0] = intervals[i][0];
+                intervals[current][1] = intervals[i][1];
+            }
+        }
+        return Arrays.copyOfRange(intervals, 0, ++current);
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q1_100/Question56Test.java
@@ -0,0 +1,26 @@
+package com.wentjiang.leetcode.q1_100;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wentao.jiang
+ * @date 2019/11/15 2:13 PM
+ * @description
+ */
+public class Question56Test {
+
+    private Question56 question56;
+
+    @Before
+    public void setUp() throws Exception {
+        question56 = new Question56();
+    }
+
+    @Test
+    public void merge() {
+        question56.merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}});
+    }
+}
