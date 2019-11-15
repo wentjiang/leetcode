88feffaf09e97facd50195a4188cefec01a549bd
wentjiang <wentjiang@sina.com>
2019-11-15 15:20:09 +0800

    improve question64

---
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question64.java
@@ -0,0 +1,25 @@
+package com.wentjiang.leetcode.q1_100;
+
+/**
+ * @author wentao.jiang
+ * @date 2019/11/15 3:10 PM
+ * @description
+ */
+public class Question64 {
+    public int minPathSum(int[][] grid) {
+        int row = grid.length;
+        int line = grid[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < line; j++) {
+                if (i > 0 && j > 0) {
+                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                } else if (i > 0) {
+                    grid[i][j] += grid[i - 1][j];
+                } else if (j > 0) {
+                    grid[i][j] += grid[i][j - 1];
+                }
+            }
+        }
+        return grid[row - 1][line - 1];
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q1_100/Question64Test.java
@@ -0,0 +1,30 @@
+package com.wentjiang.leetcode.q1_100;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wentao.jiang
+ * @date 2019/11/15 3:14 PM
+ * @description
+ */
+public class Question64Test {
+
+    private Question64 question64;
+
+    @Before
+    public void setUp() throws Exception {
+        question64 = new Question64();
+    }
+
+    @Test
+    public void minPathSum() {
+        Assert.assertEquals(7, question64.minPathSum(
+            new int[][]{{1, 3, 1},
+                {1, 5, 1},
+                {4, 2, 1}}));
+    }
+}
