ea950d5bdfe0ccec3e0adea0f9f1e63b01ce7d36
wentjiang <wentjiang@sina.com>
2020-01-12 21:52:10 +0800

    add question120 动态规划解120

---
+++ src/main/java/com/wentjiang/leetcode/q101_200/Question120.java
@@ -0,0 +1,34 @@
+package com.wentjiang.leetcode.q101_200;
+
+import java.util.List;
+
+/**
+ * @author wentao.jiang
+ * @date 2020/1/12 9:24 PM
+ * @description
+ */
+public class Question120 {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle.size() == 1) {
+            return triangle.get(0).get(0);
+        }
+        int result = Integer.MAX_VALUE;
+        for (int i = 1; i < triangle.size(); i++) {
+            List<Integer> last = triangle.get(i - 1);
+            List<Integer> now = triangle.get(i);
+            for (int j = 0; j < now.size(); j++) {
+                if (j == 0) {
+                    now.set(j, last.get(j) + now.get(j));
+                } else if (j == now.size() - 1) {
+                    now.set(j, last.get(j - 1) + now.get(j));
+                } else {
+                    now.set(j, Math.min(last.get(j), last.get(j - 1)) + now.get(j));
+                }
+                if (i == triangle.size() - 1 && result > now.get(j)) {
+                    result = now.get(j);
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q101_200/Question120Test.java
@@ -0,0 +1,34 @@
+package com.wentjiang.leetcode.q101_200;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author wentao.jiang
+ * @date 2020/1/12 9:44 PM
+ * @description
+ */
+public class Question120Test {
+
+    private Question120 question120;
+
+    @Before
+    public void setUp() throws Exception {
+        question120 = new Question120();
+    }
+
+    @Test
+    public void minimumTotal() {
+        List<List<Integer>> triangle = new ArrayList<>();
+        triangle.add(Collections.singletonList(-1));
+        triangle.add(Arrays.asList(2, 3));
+        triangle.add(Arrays.asList(1, -1, -1));
+        Assert.assertEquals(0, question120.minimumTotal(triangle));
+    }
+}
