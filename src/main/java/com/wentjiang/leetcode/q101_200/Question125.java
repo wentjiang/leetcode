b744abd51fbb18096855aef103e67ec49e21abb9
wentjiang <wentjiang@sina.com>
2020-01-25 20:40:15 +0800

    add question125 判断大小写字母不能直接用绝对值相差32,因为字母和数字的asck2编码差别也可能是32,如"0P"

---
+++ src/main/java/com/wentjiang/leetcode/q101_200/Question125.java
@@ -0,0 +1,49 @@
+package com.wentjiang.leetcode.q101_200;
+
+/**
+ * @author wentao.jiang
+ * @date 2020/1/25 5:28 PM
+ * @description
+ */
+public class Question125 {
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        char[] chars = s.toCharArray();
+        int head = 0;
+        int tail = chars.length - 1;
+        while (head < tail) {
+            if (!isCommonChar(chars[head])) {
+                head++;
+                continue;
+            }
+            if (!isCommonChar(chars[tail])) {
+                tail--;
+                continue;
+            }
+            if (chars[head] == chars[tail]
+                || isChar(chars[head]) && isChar(chars[tail])
+                && Math.abs(chars[head] - chars[tail]) == 32) {
+                head++;
+                tail--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isCommonChar(char ch) {
+        return isNumber(ch) || isChar(ch);
+    }
+
+    public boolean isNumber(char ch) {
+        return (ch >= '0' && ch <= '9');
+    }
+
+    public boolean isChar(char ch) {
+        return (ch >= 'a' && ch <= 'z')
+            || (ch >= 'A' && ch <= 'Z');
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q101_200/Question125Test.java
@@ -0,0 +1,37 @@
+package com.wentjiang.leetcode.q101_200;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wentao.jiang
+ * @date 2020/1/25 7:58 PM
+ * @description
+ */
+public class Question125Test {
+
+    private Question125 question125;
+
+    @Before
+    public void setUp() throws Exception {
+        question125 = new Question125();
+    }
+
+    @Test
+    public void isPalindrome() {
+        Assert.assertEquals(true, question125.isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    @Test
+    public void isPalindrome1() {
+        Assert.assertEquals(false, question125.isPalindrome("race a car"));
+    }
+
+    @Test
+    public void isPalindrome2() {
+        Assert.assertEquals(false, question125.isPalindrome("0P"));
+    }
+}
