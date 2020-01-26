8f069c16278f434b03f700523a0b86151bdb0a9b
wentjiang <wentjiang@sina.com>
2020-01-26 12:11:57 +0800

    add question144 使用递归的方法前序遍历树

---
+++ src/main/java/com/wentjiang/leetcode/q101_200/Question144.java
@@ -0,0 +1,37 @@
+package com.wentjiang.leetcode.q101_200;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wentao.jiang
+ * @date 2020/1/26 12:06 PM
+ * @description
+ */
+public class Question144 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        viewTree(root, result);
+        return result;
+    }
+
+    public void viewTree(TreeNode root, List<Integer> result) {
+        if (root != null) {
+            result.add(root.val);
+        } else {
+            return;
+        }
+        viewTree(root.left, result);
+        viewTree(root.right, result);
+    }
+}
