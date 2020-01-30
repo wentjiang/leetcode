dc3d9f82ec28d762b57e5fca0db9a0e6eea4bf1c
wentjiang <wentjiang@sina.com>
2020-01-30 19:30:15 +0800

    add Question153

---
+++ src/test/java/com/wentjiang/leetcode/q101_200/Question153Test.java
@@ -0,0 +1,31 @@
+package com.wentjiang.leetcode.q101_200;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wentao.jiang
+ * @date 2020/1/30 7:27 PM
+ * @description
+ */
+public class Question153Test {
+
+    private Question153 question153;
+
+    @Before
+    public void setUp() throws Exception {
+        question153 = new Question153();
+    }
+
+    @Test
+    public void findMin() {
+        assertEquals(1, question153.findMin(new int[]{3, 4, 5, 1, 2}));
+    }
+
+    @Test
+    public void findMin1() {
+        assertEquals(0, question153.findMin(new int[]{4, 5, 6, 7, 0, 1, 2}));
+    }
+}
