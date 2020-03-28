d514ed5d0d335db6ccfca52f02d36d2471844455
wentjiang <wentjiang@sina.com>
2020-03-28 16:26:24 +0800

    add Question914

---
+++ src/main/java/com/wentjiang/leetcode/q901_1000/Question914.java
@@ -0,0 +1,40 @@
+package com.wentjiang.leetcode.q901_1000;
+
+import sun.jvm.hotspot.types.CIntegerField;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Question914 {
+    public boolean hasGroupsSizeX(int[] deck) {
+        //先确定最小重复数量
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : deck) {
+            if (map.get(i) != null) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        int minRepeat = Integer.MAX_VALUE;
+        for (Map.Entry<Integer,Integer> entry:map.entrySet()){
+            minRepeat =  Math.min(entry.getValue(),minRepeat);
+        }
+        if (minRepeat < 2){
+            return false;
+        }
+        int num = map.size();
+        for (int i = 2;i<=minRepeat;i++){
+            int temp = 0;
+            for (Map.Entry<Integer,Integer> entry:map.entrySet()){
+                if (entry.getValue() % i == 0){
+                    temp+=1;
+                }
+            }
+            if (temp == num){
+                return true;
+            }
+        }
+        return false;
+    }
+}
