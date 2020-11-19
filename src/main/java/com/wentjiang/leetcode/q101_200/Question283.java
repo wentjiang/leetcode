713dce1d9e254f5b00846162f1ff544fbe7c5e5e
wentjiang <wentjiang@sina.com>
2020-11-20 00:14:13 +0800

    add Question844_3 & 283

---
+++ src/main/java/com/wentjiang/leetcode/q101_200/Question283.java
@@ -0,0 +1,32 @@
+package com.wentjiang.leetcode.q101_200;
+
+public class Question283 {
+    /**
+     * 一次遍历法
+     */
+    public void moveZeroes1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == 0 && nums[j] != 0) {
+                    nums[i] = nums[j];
+                    nums[j] = 0;
+                }
+            }
+        }
+    }
+
+    /**
+     * 双指针法
+     */
+    public void moveZeroes2(int[] nums) {
+        int notZeroNum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[notZeroNum++] = nums[i];
+            }
+        }
+        for (int i = notZeroNum; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q801_900/Question844_3Test.java
@@ -0,0 +1,40 @@
+package com.wentjiang.leetcode.q801_900;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class Question844_3Test {
+    private Question844_3 question844 = new Question844_3();
+
+    @Test
+    public void test_case1() {
+        Assertions.assertTrue(question844.backspaceCompare("ab##", "c#d#"));
+    }
+
+    @Test
+    public void test_case2() {
+        Assertions.assertTrue(question844.backspaceCompare("a##c", "#a#c"));
+    }
+
+    @Test
+    public void test_case3() {
+        Assertions.assertFalse(question844.backspaceCompare("a#c", "b"));
+    }
+
+    @Test
+    public void test_case4() {
+        Assertions.assertTrue(question844.backspaceCompare("xywrrmp", "xywrrmu#p"));
+    }
+
+    @Test
+    public void test_case5() {
+        Assertions.assertTrue(question844.backspaceCompare("bxj##tw", "bxo#j##tw"));
+    }
+
+    @Test
+    public void test_case6() {
+        Assertions.assertFalse(question844.backspaceCompare("bxj##tw", "bxj###tw"));
+    }
+
+
+}
