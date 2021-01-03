5e15820300ba64856a3e16d430b2b6e3d6947e3a
wentjiang <wentjiang@sina.com>
2021-01-03 21:08:35 +0800

    add question86

---
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question86.java
@@ -0,0 +1,26 @@
+package com.wentjiang.leetcode.q1_100;
+
+import com.wentjiang.leetcode.utils.ListNode;
+
+public class Question86 {
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode smallHead = small;
+        ListNode large = new ListNode(0);
+        ListNode largeHead = large;
+        while(head != null){
+            if (head.val < x){
+                small.next = head;
+                small = head;
+            }else{
+                large.next = head;
+                large = head;
+            }
+            head = head.next;
+        }
+        small.next = largeHead.next;
+        large.next = null;
+        head = smallHead.next;
+        return head;
+    }
+}
