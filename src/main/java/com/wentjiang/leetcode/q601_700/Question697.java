b130ac13528b1211042b61a03e86f01c157fca5f
wentaojiang <wentjiang@sina.com>
2021-02-20 13:12:07 +0800

    add Question697

---
+++ src/main/java/com/wentjiang/leetcode/q601_700/Question697.java
@@ -0,0 +1,53 @@
+package com.wentjiang.leetcode.q601_700;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Question697 {
+    public int findShortestSubArray(int[] nums) {
+        if (nums.length < 1) {
+            return nums.length;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxCount = Integer.MIN_VALUE;
+        //先找到众数
+        for (int num : nums) {
+            int value = map.getOrDefault(num, 0) + 1;
+            map.put(num, value);
+            maxCount = Math.max(maxCount, value);
+        }
+
+        if (maxCount == 1) {
+            return 1;
+        }
+        List<Integer> maxCountNums = new ArrayList<>();
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == maxCount) {
+                maxCountNums.add(entry.getKey());
+            }
+        }
+        int result = Integer.MAX_VALUE;
+        //找到当前数第一个和最后一个的坐标差值
+        for (int num : maxCountNums) {
+            int first = 0;
+            int last = nums.length - 1;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == num) {
+                    first = i;
+                    break;
+                }
+            }
+            for (int i = nums.length - 1; i >= 0; i--) {
+                if (nums[i] == num) {
+                    last = i;
+                    break;
+                }
+            }
+            result = Math.min(last - first + 1, result);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q601_700/Question697Test.java
@@ -0,0 +1,19 @@
+package com.wentjiang.leetcode.q601_700;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Question697Test {
+
+    private Question697 question697 = new Question697();
+
+    @Test
+    void findShortestSubArray() {
+        Assertions.assertEquals(6, question697.findShortestSubArray(new int[]{1, 2, 2, 3, 1, 4, 2}));
+    }
+
+    @Test
+    void findShortestSubArray_1() {
+        Assertions.assertEquals(2, question697.findShortestSubArray(new int[]{1, 2, 2, 3, 1}));
+    }
+}
