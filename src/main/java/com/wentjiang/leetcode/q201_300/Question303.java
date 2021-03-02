42b27ddffe6003c0b065419503a6bead1a4820fb
wentjiang <wentjiang@sina.com>
2021-03-02 21:00:49 +0800

    add Question303

--- src/main/java/com/wentjiang/leetcode/q201_300/Question303.java
+++ src/main/java/com/wentjiang/leetcode/q201_300/Question303.java
@@ -19,3 +19,24 @@ class NumArray {
         return sum;
     }
 }
+
+class NumArray1 {
+
+    private int[] sums;
+
+    public NumArray1(int[] nums) {
+        sums = new int[nums.length];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            sums[i] = sum;
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        if (i == 0) {
+            return sums[j];
+        }
+        return sums[j] - sums[i - 1];
+    }
+}
