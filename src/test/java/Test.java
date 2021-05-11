5bb059ede7bcde804741544964a7853be3dcd046
wentjiang <wentjiang@sina.com>
2021-05-11 20:25:41 +0800

    update test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -31,10 +31,7 @@ public class Test {
     }
 
     public static void main(String[] args) {
-//        StringTokenizer stringTokenizer = new StringTokenizer("aaa bbb");
-        System.out.println(Math.pow(2, 3));
-        System.out.println(Math.pow(3, 2));
-
+        System.out.println(1 ^ 2);
     }
 
 }
