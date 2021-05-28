fe7e2e09c74c149035f778cd3bf4bbe0f7dc97d7
wentjiang <wentjiang@sina.com>
2021-05-28 15:45:46 +0800

    add Questino1190 and add junit ParameterizedTest

---
+++ src/main/java/com/wentjiang/leetcode/q1101_1200/Question1190.java
@@ -0,0 +1,29 @@
+package com.wentjiang.leetcode.q1101_1200;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Question1190 {
+    public String reverseParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == ')') {
+                char top = stack.pop();
+                List<Character> tempList = new ArrayList<>();
+                while(top != '('){
+                    tempList.add(top);
+                    top = stack.pop();
+                }
+                stack.addAll(tempList);
+            } else {
+                stack.add(ch);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char ch : stack) {
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q1101_1200/Question1190Test.java
@@ -0,0 +1,30 @@
+package com.wentjiang.leetcode.q1101_1200;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+public class Question1190Test {
+
+    private Question1190 question1190 = new Question1190();
+
+    @ParameterizedTest
+    @MethodSource("resultAndParam")
+    public void reverseParentheses(String result, String param) {
+        Assertions.assertEquals(result, question1190.reverseParentheses(param));
+    }
+
+    static Stream<Arguments> resultAndParam() {
+        return Stream.of(
+                Arguments.arguments("dcba", "(abcd)"),
+                Arguments.arguments("iloveu", "(u(love)i)"),
+                Arguments.arguments("leetcode", "(ed(et(oc))el)"),
+                Arguments.arguments("apmnolkjihgfedcbq", "a(bcdefghijkl(mno)p)q"));
+    }
+}
