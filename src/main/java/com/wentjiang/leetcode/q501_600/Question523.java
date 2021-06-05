2ccb1af9c76af7f0ddd6cf122ad5d7baa23558f1
wentjiang <wentjiang@sina.com>
2021-06-05 22:59:36 +0800

    add Question523

--- src/main/java/com/wentjiang/leetcode/q501_600/Question523.java
+++ src/main/java/com/wentjiang/leetcode/q501_600/Question523.java
@@ -1,6 +1,31 @@
 package com.wentjiang.leetcode.q501_600;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Question523 {
+    /**
+     * 循环遍历,暴力法 O(n^2)
+     */
+    public boolean checkSubarraySum1(int[] nums, int k) {
+        int length = nums.length;
+        if (length < 2) {
+            return false;
+        }
+        for (int i = 0; i < length; i++) {
+            int sum = nums[i];
+            for (int j = i + 1; j < length; j++) {
+                sum += nums[j];
+                if (sum % k == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     /**
      * 前缀法,issue:there are too many data cause timeout error
      */
@@ -15,6 +40,28 @@ public class Question523 {
                 if ((temps[j] - temps[i]) % k == 0) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * 余数+哈希表方法
+     */
+    public boolean checkSubarraySum2(int[] nums, int k) {
+        Map<Integer, Integer> moduloAndIndex = new HashMap<>();
+        moduloAndIndex.put(0, -1);
+        int remainder = 0;
+        for (int i = 0; i < nums.length; i++) {
+            remainder = (remainder + nums[i]) % k;
+            if (moduloAndIndex.containsKey(remainder)) {
+                int prevIndex = moduloAndIndex.get(remainder);
+                if (i - prevIndex >= 2) {
+                    return true;
+                }
+            } else {
+                moduloAndIndex.put(remainder, i);
             }
         }
         return false;
--- src/test/java/com/wentjiang/leetcode/q501_600/Question523Test.java
+++ src/test/java/com/wentjiang/leetcode/q501_600/Question523Test.java
@@ -1,6 +1,5 @@
 package com.wentjiang.leetcode.q501_600;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -14,8 +13,20 @@ public class Question523Test {
 
     @ParameterizedTest
     @MethodSource("resultAndParams")
+    public void checkSubarraySum1(boolean result, int[] nums, int k) {
+        Assertions.assertEquals(result, question523.checkSubarraySum1(nums, k));
+    }
+
+    @ParameterizedTest
+    @MethodSource("resultAndParams")
     public void checkSubarraySum(boolean result, int[] nums, int k) {
         Assertions.assertEquals(result, question523.checkSubarraySum(nums, k));
+    }
+
+    @ParameterizedTest
+    @MethodSource("resultAndParams")
+    public void checkSubarraySum2(boolean result, int[] nums, int k) {
+        Assertions.assertEquals(result, question523.checkSubarraySum2(nums, k));
     }
 
     static Stream<Arguments> resultAndParams() {
@@ -23,12 +34,10 @@ public class Question523Test {
                 Arguments.arguments(true, new int[]{23, 2, 4, 6, 7}, 6),
                 Arguments.arguments(true, new int[]{23, 2, 6, 4, 7}, 6),
                 Arguments.arguments(false, new int[]{23, 2, 6, 4, 7}, 13),
-                Arguments.arguments(true, new int[]{23, 2, 4, 6, 6}, 7)
+                Arguments.arguments(true, new int[]{23, 2, 4, 6, 6}, 7),
+                Arguments.arguments(true, new int[]{2, 4, 3}, 6),
+                Arguments.arguments(false, new int[]{0}, 1),
+                Arguments.arguments(true, new int[]{0, 0}, 1)
         );
-    }
-
-    @Test
-    public void test1(){
-        Assertions.assertTrue(question523.checkSubarraySum(new int[]{23, 2, 4, 6, 6}, 7));
     }
 }
