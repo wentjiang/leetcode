c93d34ed7cc6cfc019f44935c24bad7912ba3b95
wentjiang <wentjiang@sina.com>
2021-06-14 20:53:25 +0800

    add question374

---
+++ src/main/java/com/wentjiang/leetcode/q301_400/Question374.java
@@ -0,0 +1,43 @@
+package com.wentjiang.leetcode.q301_400;
+
+public class Question374 {
+
+    private final int guessNum;
+
+    public Question374(int guessNum) {
+        this.guessNum = guessNum;
+    }
+
+    public int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            int result = guess(mid);
+            if (result <= 0){
+                end = mid;
+            }else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    public int guessNumber1(int n) {
+        int left = 1, right = n;
+        while (left < right) { // 循环直至区间左右端点相同
+            int mid = left + (right - left) / 2; // 防止计算时溢出
+            if (guess(mid) <= 0) {
+                right = mid; // 答案在区间 [left, mid] 中
+            } else {
+                left = mid + 1; // 答案在区间 [mid+1, right] 中
+            }
+        }
+        // 此时有 left == right，区间缩为一个点，即为答案
+        return left;
+    }
+
+    public int guess(int num) {
+        return -Integer.compare(num, guessNum);
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q301_400/Question374Test.java
@@ -0,0 +1,39 @@
+package com.wentjiang.leetcode.q301_400;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.Assert.*;
+
+public class Question374Test {
+
+    @Test
+    public void guessNumber() {
+        Question374 question374 = new Question374(6);
+        Assertions.assertEquals(6, question374.guessNumber(10));
+    }
+
+    @Test
+    public void guessNumber1(){
+        Question374 question374 = new Question374(1);
+        Assertions.assertEquals(1, question374.guessNumber(1));
+    }
+
+    @Test
+    public void guessNumber2(){
+        Question374 question374 = new Question374(1);
+        Assertions.assertEquals(1, question374.guessNumber(2));
+    }
+
+    @Test
+    public void guessNumber3(){
+        Question374 question374 = new Question374(2);
+        Assertions.assertEquals(2, question374.guessNumber(2));
+    }
+
+    @Test
+    public void guessNumber4(){
+        Question374 question374 = new Question374(1);
+        Assertions.assertEquals(1, question374.guessNumber(3));
+    }
+}
