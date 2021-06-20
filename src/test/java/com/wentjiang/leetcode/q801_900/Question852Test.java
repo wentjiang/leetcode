fd35fea2034b7292649cbdf1ba1b3eb1e74775e9
wentjiang <wentjiang@sina.com>
2021-06-20 18:13:14 +0800

    add Question852

---
+++ src/test/java/com/wentjiang/leetcode/q801_900/Question852Test.java
@@ -0,0 +1,26 @@
+package com.wentjiang.leetcode.q801_900;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.Assert.*;
+
+public class Question852Test {
+
+    private Question852 question852 = new Question852();
+
+    @Test
+    public void peakIndexInMountainArray() {
+        Assertions.assertEquals(1, question852.peakIndexInMountainArray(new int[]{0, 1, 0}));
+    }
+
+    @Test
+    public void peakIndexInMountainArray1() {
+        Assertions.assertEquals(1, question852.peakIndexInMountainArray(new int[]{0, 2, 1, 0}));
+    }
+
+    @Test
+    public void peakIndexInMountainArray2() {
+        Assertions.assertEquals(2, question852.peakIndexInMountainArray(new int[]{24, 69, 100, 99, 79, 78, 67, 36, 26, 19}));
+    }
+}
