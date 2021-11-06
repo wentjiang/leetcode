e3cd5cacb711cb2f4967c237ce96dc6724fe6057
wentjiang <wentjiang@sina.com>
2021-11-06 12:32:19 +0800

    add question268

---
+++ src/main/java/com/wentjiang/leetcode/q201_300/Question268.java
@@ -0,0 +1,18 @@
+package com.wentjiang.leetcode.q201_300;
+
+import java.util.Arrays;
+
+public class Question268 {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        if (nums[0] != 0){
+            return 0;
+        }
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1] - 1) {
+                return i + 1;
+            }
+        }
+        return nums.length;
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q201_300/Question268Test.java
@@ -0,0 +1,34 @@
+package com.wentjiang.leetcode.q201_300;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class Question268Test {
+    private Question268 question268 = new Question268();
+
+    @Test
+    public void testMissingNumber1() {
+        Assertions.assertEquals(2, question268.missingNumber(new int[]{3, 0, 1}));
+    }
+
+    @Test
+    public void testMissingNumber2() {
+        Assertions.assertEquals(2, question268.missingNumber(new int[]{0, 1}));
+    }
+
+    @Test
+    public void testMissingNumber3() {
+        Assertions.assertEquals(8, question268.missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}));
+    }
+
+    @Test
+    public void testMissingNumber4() {
+        Assertions.assertEquals(1, question268.missingNumber(new int[]{0}));
+    }
+
+    @Test
+    public void testMissingNumber5() {
+        Assertions.assertEquals(0, question268.missingNumber(new int[]{1}));
+    }
+
+}
