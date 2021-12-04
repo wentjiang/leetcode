19a554d5ffe795e96e4bdff18bd5cd76648a18d7
wentjiang <wentjiang@sina.com>
2021-12-04 17:34:17 +0800

    add question 383

---
+++ src/main/java/com/wentjiang/leetcode/q301_400/Question383.java
@@ -0,0 +1,33 @@
+package com.wentjiang.leetcode.q301_400;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Question383 {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character,Integer> magazineMap = transformStirng2Map(magazine);
+        for (char ch:ransomNote.toCharArray()){
+            if (!magazineMap.containsKey(ch) || magazineMap.get(ch) <= 0){
+                return false;
+            } else {
+                magazineMap.put(ch,magazineMap.get(ch) - 1);
+            }
+        }
+
+        return true;
+    }
+
+    private Map<Character,Integer> transformStirng2Map(String magazine){
+        Map<Character, Integer> result = new HashMap<>();
+        for (char ch:magazine.toCharArray()){
+            if (result.containsKey(ch)){
+                result.put(ch,result.get(ch) + 1);
+            }else{
+                result.put(ch,1);
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/com/wentjiang/leetcode/q301_400/Question383Test.java
@@ -0,0 +1,27 @@
+package com.wentjiang.leetcode.q301_400;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Question383Test {
+
+    private Question383 question383 = new Question383();
+
+    @Test
+    void canConstruct1() {
+        Assertions.assertFalse(question383.canConstruct("a","b"));
+    }
+
+    @Test
+    void canConstruct2() {
+        Assertions.assertFalse(question383.canConstruct("aa","ab"));
+    }
+
+    @Test
+    void canConstruct3() {
+        Assertions.assertTrue(question383.canConstruct("aa","aab"));
+    }
+
+}
