829488eb5c99fc93b249c77e32f00451e58e29c7
wentjiang <wentjiang@sina.com>
2021-12-18 12:16:54 +0800

    add question419

---
+++ src/main/java/com/wentjiang/leetcode/q401_500/Question419.java
@@ -0,0 +1,25 @@
+package com.wentjiang.leetcode.q401_500;
+
+public class Question419 {
+    public int countBattleships(char[][] board) {
+        int lineNum = board.length;
+        int colNum = board[0].length;
+        int sum = 0;
+
+        for (int i = 0; i < lineNum; i++) {
+            for (int j = 0; j < colNum; j++) {
+                if (board[i][j]=='X'){
+                    board[i][j]='.';
+                    for (int k = j + 1; k < colNum && board[i][k] == 'X'; ++k) {
+                        board[i][k] = '.';
+                    }
+                    for (int k = i + 1; k < lineNum && board[k][j] == 'X'; ++k) {
+                        board[k][j] = '.';
+                    }
+                    sum++;
+                }
+            }
+        }
+        return sum;
+    }
+}
