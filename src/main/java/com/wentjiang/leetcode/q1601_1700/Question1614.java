3366ff5c5fc2515681d0d658a154bbad5f41aced
wentjiang <wentjiang@sina.com>
2022-01-08 10:19:57 +0800

    add question1614

---
+++ src/main/java/com/wentjiang/leetcode/q1601_1700/Question1614.java
@@ -0,0 +1,22 @@
+package com.wentjiang.leetcode.q1601_1700;
+
+public class Question1614 {
+    public int maxDepth(String s) {
+        int maxDepth = 0;
+        int depth = 0;
+        for (char ch: s.toCharArray()){
+            switch (ch){
+                case '(':
+                    depth++;
+                    maxDepth = Math.max(depth,maxDepth);
+                    break;
+                case ')':
+                    depth--;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return maxDepth;
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q1601_1700/Question1614Test.java
@@ -0,0 +1,31 @@
+package com.wentjiang.leetcode.q1601_1700;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Question1614Test {
+
+    private Question1614 question1614 = new Question1614();
+
+    @Test
+    void maxDepth() {
+        Assertions.assertEquals(3,question1614.maxDepth("(1+(2*3)+((8)/4))+1"));
+    }
+
+    @Test
+    void maxDepth1() {
+        Assertions.assertEquals(3,question1614.maxDepth("(1)+((2))+(((3)))"));
+    }
+
+    @Test
+    void maxDepth2() {
+        Assertions.assertEquals(1,question1614.maxDepth("1+(2*3)/(2-1)"));
+    }
+
+    @Test
+    void maxDepth3() {
+        Assertions.assertEquals(0,question1614.maxDepth("1"));
+    }
+}
