75ffff017181b4b79160e9f9b066f82986f0a0ca
wentjiang <wentjiang@sina.com>
2022-03-13 11:29:18 +0800

    add question393

---
+++ src/main/java/com/wentjiang/leetcode/q301_400/Question393.java
@@ -0,0 +1,62 @@
+package com.wentjiang.leetcode.q301_400;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question393 {
+    public boolean validUtf8(int[] data) {
+        List<String> list = new ArrayList<>(data.length);
+        for (int num : data) {
+            String binaryString = Integer.toBinaryString(num);
+            if (binaryString.length() < 8) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < 8 - binaryString.length(); i++) {
+                    sb.append(0);
+                }
+                sb.append(binaryString);
+                list.add(sb.toString());
+            } else {
+                list.add(binaryString);
+            }
+        }
+        int index = 0;
+        while (true) {
+            String str = list.get(index);
+            //0开头,只有一个字节
+            if (list.get(index).startsWith("0")) {
+                index++;
+                //1开头,有多个字节
+            } else if (list.get(index).startsWith("10")) {
+                return false;
+            } else {
+                char[] chars = str.toCharArray();
+                int oneNum = 0;
+                for (char ch : chars) {
+                    if (ch == '1') {
+                        oneNum++;
+                    } else {
+                        break;
+                    }
+                    if (oneNum > 4) {
+                        return false;
+                    }
+                }
+                for (int i = 1; i < oneNum; i++) {
+                    if (index + i >= data.length) {
+                        return false;
+                    } else {
+                        if (!list.get(index + i).startsWith("10")) {
+                            return false;
+                        }
+                    }
+                }
+                index += oneNum;
+            }
+
+            if (index >= data.length) {
+                break;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q301_400/Question393Test.java
@@ -0,0 +1,31 @@
+package com.wentjiang.leetcode.q301_400;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Question393Test {
+
+    private Question393 question393 = new Question393();
+
+    @Test
+    void validUtf8() {
+        Assertions.assertTrue(question393.validUtf8(new int[]{197,130,1}));
+    }
+
+    @Test
+    void validUtf8_1() {
+        Assertions.assertFalse(question393.validUtf8(new int[]{235,140,4}));
+    }
+
+    @Test
+    void validUtf8_2() {
+        Assertions.assertFalse(question393.validUtf8(new int[]{237}));
+    }
+
+    @Test
+    void validUtf8_3() {
+        Assertions.assertFalse(question393.validUtf8(new int[]{145}));
+    }
+}
