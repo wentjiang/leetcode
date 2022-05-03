2cfffb273c1f875a3c0e591e43c4912f4aa6e901
wentjiang <wentjiang@sina.com>
2022-05-03 23:24:13 +0800

    add question937

---
+++ src/main/java/com/wentjiang/leetcode/q901_1000/Question937.java
@@ -0,0 +1,33 @@
+package com.wentjiang.leetcode.q901_1000;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Question937 {
+    public String[] reorderLogFiles(String[] logs) {
+        List<String> stringLogs = new ArrayList<>();
+        List<String> numberLogs = new ArrayList<>();
+        for (String log : logs) {
+            String[] strings = log.split(" ");
+            Matcher isNum = Pattern.compile("-?[0-9]+").matcher(strings[1]);
+            if (isNum.matches()) {
+                numberLogs.add(log);
+            } else {
+                stringLogs.add(log);
+            }
+        }
+        stringLogs.sort((o1, o2) -> {
+            String subString1 = o1.substring(o1.indexOf(" ") + 1);
+            String subString2 = o2.substring(o2.indexOf(" ") + 1);
+            if (subString1.equals(subString2)) {
+                return o1.split(" ")[0].compareTo(o2.split(" ")[0]);
+            } else {
+                return subString1.compareTo(subString2);
+            }
+        });
+        stringLogs.addAll(numberLogs);
+        return stringLogs.toArray(new String[]{});
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q901_1000/Question937Test.java
@@ -0,0 +1,25 @@
+package com.wentjiang.leetcode.q901_1000;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Question937Test {
+
+    private Question937 question937 = new Question937();
+
+    @Test
+    void reorderLogFiles() {
+        Assertions.assertArrayEquals(new String[]{"let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6"},
+                question937.reorderLogFiles(new String[]{"dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"}));
+    }
+
+    @Test
+    void reorderLogFiles1() {
+        Assertions.assertArrayEquals(new String[]{"g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"},
+                question937.reorderLogFiles(new String[]{"a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"}));
+    }
+
+}
