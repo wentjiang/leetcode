0fae136053d198851ac25a2e9d5117728991453e
wentjiang <wentjiang@sina.com>
2023-06-06 22:08:18 +0800

    add day1Question3

---
+++ src/main/java/com/wentjiang/leetcode/practice/day1/Day1Question3.java
@@ -0,0 +1,27 @@
+package com.wentjiang.leetcode.practice.day1;
+
+//最短路径和
+public class Day1Question3 {
+
+    //用一张额外的二维数组表记录当前节点的最短距离
+    public int getShortestPath(int[][] grid) {
+        int result = 0;
+        int rowNum = grid.length;
+        int columnNum = grid[0].length;
+        int[][] shortestPath = new int[rowNum][columnNum];
+        for (int i = 0; i < rowNum; i++) {
+            for (int j = 0; j < columnNum; j++) {
+                if (i == 0 && j == 0) {
+                    shortestPath[i][j] = grid[i][j];
+                } else if (i == 0) {
+                    shortestPath[i][j] = grid[i][j] + shortestPath[i][j - 1];
+                } else if (j == 0) {
+                    shortestPath[i][j] = grid[i][j] + shortestPath[i - 1][j];
+                } else {
+                    shortestPath[i][j] = grid[i][j] + Math.min(shortestPath[i - 1][j], shortestPath[i][j - 1]);
+                }
+            }
+        }
+        return shortestPath[rowNum - 1][columnNum - 1];
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/practice/day1/Day1Question3Test.java
@@ -0,0 +1,28 @@
+package com.wentjiang.leetcode.practice.day1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Day1Question3Test {
+
+    private Day1Question3 day1Question3 = new Day1Question3();
+
+    @Test
+    public void case1() {
+        int result = day1Question3.getShortestPath(new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}});
+        Assertions.assertEquals(7, result);
+    }
+
+    @Test
+    public void case2() {
+        int result = day1Question3.getShortestPath(new int[][]{{1, 2, 3}, {4, 5, 6}});
+        Assertions.assertEquals(12, result);
+    }
+
+    @Test
+    public void case3() {
+        int result = day1Question3.getShortestPath(new int[][]{{1}});
+        Assertions.assertEquals(1, result);
+    }
+
+}
