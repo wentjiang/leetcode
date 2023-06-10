3aba6b729bc812df2061246515c27cc8f9e6d4a1
wentjiang <wentjiang@sina.com>
2023-06-10 13:16:48 +0800

    add z output

--- src/main/java/com/wentjiang/leetcode/practice/ZOutput.java
+++ src/main/java/com/wentjiang/leetcode/practice/ZOutput.java
@@ -1,2 +1,46 @@
-package com.wentjiang.leetcode.practice;public class ZOutput {
+package com.wentjiang.leetcode.practice;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+//Z字形输出字符串
+public class ZOutput {
+
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * @param str string字符串
+     * @param k   int整型
+     * @return string字符串
+     */
+    public String zconvert(String str, int k) {
+        if (str.length() <= 2) {
+            return str;
+        }
+        boolean down = false;
+        int currentRowNum = 0;
+        char[] chars = str.toCharArray();
+        List<StringBuilder> sbList = new ArrayList<>(k);
+        for (int i = 0; i < k; i++) {
+            sbList.add(new StringBuilder());
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (currentRowNum == 0 || currentRowNum == k - 1) {
+                down = !down;
+            }
+            sbList.get(currentRowNum).append(chars[i]);
+            if (down) {
+                currentRowNum++;
+            } else {
+                currentRowNum--;
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < k; i++) {
+            result = result.append(sbList.get(i));
+        }
+        return result.toString();
+    }
+
 }
--- src/test/java/com/wentjiang/leetcode/practice/ZOutputTest.java
+++ src/test/java/com/wentjiang/leetcode/practice/ZOutputTest.java
@@ -1,4 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.wentjiang.leetcode.practice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class ZOutputTest {
-  
+
+    private ZOutput zOutput = new ZOutput();
+
+    @Test
+    public void case1() {
+        String result = zOutput.zconvert("nowcoder", 3);
+        Assertions.assertEquals("noocdrwe", result);
+    }
+
+    @Test
+    public void case2() {
+        String result = zOutput.zconvert("nowcoder", 4);
+        Assertions.assertEquals("neodrwoc", result);
+    }
+
+    @Test
+    public void case3() {
+        String result = zOutput.zconvert("n", 4);
+        Assertions.assertEquals("n", result);
+    }
+
+
+    @Test
+    public void case4() {
+        String result = zOutput.zconvert("no", 4);
+        Assertions.assertEquals("no", result);
+    }
 }
