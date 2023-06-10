9c858345256a404e50b61b25f8d249d318807a64
wentjiang <wentjiang@sina.com>
2023-06-10 12:16:48 +0800

    add stock purchase

--- src/main/java/com/wentjiang/leetcode/practice/StockPurchase.java
+++ src/main/java/com/wentjiang/leetcode/practice/StockPurchase.java
@@ -1,8 +1,49 @@
 package com.wentjiang.leetcode.practice;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+//售卖股票最高价
 public class StockPurchase {
 
-    public int stockPurchase(int[] prices){
-        return 0;
+    public static int stockPurchase(int[] prices) {
+        int result = 0;
+        if (prices.length == 0 || prices.length == 1) {
+            return result;
+        }
+        int currentMax = Integer.MIN_VALUE;
+        int currentMin = prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            // 后一个大于等于前一个的情况
+            if (prices[i - 1] <= prices[i]) {
+                currentMax = prices[i];
+                // 小于的情况,生成新的区间
+            } else if (prices[i - 1] > prices[i]) {
+                if (currentMax > currentMin) {
+                    result = Math.max(currentMax - currentMin, result);
+                }
+                currentMin = Math.min(prices[i], currentMin);
+                currentMax = Integer.MIN_VALUE;
+            }
+        }
+
+        if (currentMax > currentMin) {
+            result = Math.max(currentMax - currentMin, result);
+        }
+
+        return result;
     }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        while (in.hasNextLine()) { // 注意 while 处理多个 case
+            String a = in.nextLine();
+            String b = in.nextLine();
+            int[] arrays = Arrays.stream(b.split(" ")).mapToInt(Integer::parseInt).toArray();
+            System.out.println(stockPurchase(arrays));
+        }
+    }
+
 }
--- src/test/java/com/wentjiang/leetcode/practice/StockPurchaseTest.java
+++ src/test/java/com/wentjiang/leetcode/practice/StockPurchaseTest.java
@@ -9,26 +9,44 @@ class StockPurchaseTest {
 
     @Test
     public void case1() {
-        int result = stockPurchase.stockPurchase(new int[]{1, 2, 3});
+        int result = stockPurchase.stockPurchase(new int[] { 1, 2, 3 });
         Assertions.assertEquals(2, result);
     }
 
     @Test
     public void case2() {
-        int result = stockPurchase.stockPurchase(new int[]{3, 2, 1});
+        int result = stockPurchase.stockPurchase(new int[] { 3, 2, 1 });
         Assertions.assertEquals(0, result);
     }
 
     @Test
     public void case3() {
-        int result = stockPurchase.stockPurchase(new int[]{3, 1, 2});
+        int result = stockPurchase.stockPurchase(new int[] { 3, 1, 2 });
         Assertions.assertEquals(1, result);
     }
 
     @Test
     public void case4() {
-        int result = stockPurchase.stockPurchase(new int[]{3, 1, 2, 4, 1, 2});
-        Assertions.assertEquals(4, result);
+        int result = stockPurchase.stockPurchase(new int[] { 3, 1, 2, 4, 1, 2 });
+        Assertions.assertEquals(3, result);
+    }
+
+    @Test
+    public void case5() {
+        int result = stockPurchase.stockPurchase(new int[] { 3, 3, 3, 3, 3, 2, 3 });
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    public void case6() {
+        int result = stockPurchase.stockPurchase(new int[] { 1, 9, 6, 9, 1, 7, 1, 1, 5, 9, 9, 9 });
+        Assertions.assertEquals(8, result);
+    }
+
+    @Test
+    public void case7() {
+        int result = stockPurchase.stockPurchase(new int[] { 7, 1, 5, 3, 6, 4 });
+        Assertions.assertEquals(5, result);
     }
 
 }
