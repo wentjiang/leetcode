be79ff752adb9ee0350816aa32752d189a33f748
wentjiang <wentjiang@sina.com>
2023-06-18 17:17:03 +0800

    add question96

---
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question96.java
@@ -0,0 +1,21 @@
+package com.wentjiang.leetcode.q1_100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Question96 {
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        //超过两个节点的情况下遍历
+        for (int i = 2; i <= n; ++i) {
+            //以当前j为根节点,分成左右的树,所有的可能性为左子树*右子树
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q1_100/Question96Test.java
@@ -0,0 +1,14 @@
+package com.wentjiang.leetcode.q1_100;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Question96Test {
+    private Question96 question96 = new Question96();
+
+    @Test
+    public void case1() {
+        Assertions.assertEquals(1, question96.numTrees(1));
+    }
+
+}
