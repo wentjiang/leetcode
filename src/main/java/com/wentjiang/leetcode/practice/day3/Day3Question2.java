054596c2440a0e8ff0a1b844c1d4408af23597c8
wentjiang <wentjiang@sina.com>
2023-06-19 23:52:00 +0800

    optimize Day3Question2

--- src/main/java/com/wentjiang/leetcode/practice/day3/Day3Question2.java
+++ src/main/java/com/wentjiang/leetcode/practice/day3/Day3Question2.java
@@ -3,25 +3,20 @@ package com.wentjiang.leetcode.practice.day3;
 public class Day3Question2 {
 
     //暴力求解法,二维数组存储当前列到之前某列的最小值,最小值*宽度即为当前的结果.
-    public int question(int[] height) {
-        if (height.length == 1) {
-            return height[0];
+    public int question(int[] heights) {
+        if (heights.length == 1) {
+            return heights[0];
         }
-        int[][] min = new int[height.length][height.length];
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < min.length; i++) {
-            for (int j = 0; j < min[0].length; j++) {
-                min[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        for (int i = 0; i < height.length; i++) {
-            for (int j = i; j < height.length; j++) {
+        for (int i = 0; i < heights.length; i++) {
+            int minHeight = Integer.MAX_VALUE;
+            for (int j = i; j < heights.length; j++) {
+                minHeight = Math.min(minHeight, heights[j]);
                 if (i == j) {
-                    min[i][j] = height[i];
+                    max = Math.max(max, minHeight);
                 } else {
-                    min[i][j] = Math.min(min[i][j - 1], height[j]);
+                    max = Math.max(max, (j - i + 1) * minHeight);
                 }
-                max = Math.max(max, (j - i + 1) * min[i][j]);
             }
         }
         return max;
