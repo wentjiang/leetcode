3bffdb9c8f4dc60afba68c7110702fd93efe6792
wentjiang <wentjiang@sina.com>
2023-07-05 22:37:42 +0800

    add question698

---
+++ src/main/java/com/wentjiang/leetcode/q601_700/Question698.java
@@ -0,0 +1,51 @@
+package com.wentjiang.leetcode.q601_700;
+
+public class Question698 {
+
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % k != 0) {
+            return false;
+        }
+        int target = sum / k;
+        int[] buckets = new int[k];
+        return backtrack(buckets, 0, nums, target);
+    }
+
+    private boolean backtrack(int[] buckets, int index, int[] nums, int target) {
+        if (index == nums.length) {
+            for (int bucket : buckets) {
+                if (bucket != target) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        for (int i = 0; i < buckets.length; i++) {
+            //超过target的时候跳过
+            if (nums[index] + buckets[i] > target) {
+                continue;
+            }
+            //当前桶子里的值和上一个桶子里的值相同时跳过,因为放在当前和上一个是一样的
+            if (i>0 && buckets[i] == buckets[i-1]){
+                continue;
+            }
+
+            //给当前的桶子增加当前的值
+            buckets[i] = buckets[i] + nums[index];
+            if (backtrack(buckets, index+1, nums, target)) {
+                return true;
+            }
+            //回溯,给当前的桶子减去当前index的值
+            buckets[i] = buckets[i] - nums[index];
+        }
+
+        return false;
+    }
+
+
+}
---
+++ src/test/java/com/wentjiang/leetcode/q601_700/Question698Test.java
@@ -0,0 +1,27 @@
+package com.wentjiang.leetcode.q601_700;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Question698Test {
+
+    private Question698 question698 = new Question698();
+
+    @Test
+    public void case1(){
+        Assertions.assertTrue(question698.canPartitionKSubsets(new int[]{4, 3, 2, 3, 5, 2, 1},4));
+    }
+
+    @Test
+    public void case2(){
+        Assertions.assertFalse(question698.canPartitionKSubsets(new int[]{1,2,3,4},3));
+    }
+
+    @Test
+    public void case3(){
+        Assertions.assertFalse(question698.canPartitionKSubsets(new int[]{3,3,10,2,6,5,10,6,8,3,2,1,6,10,7,2},6));
+    }
+
+}
