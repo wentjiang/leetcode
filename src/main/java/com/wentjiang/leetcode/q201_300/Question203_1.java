55cde3c6f06f2816bd2aceed814f0cccc81f2e6a
wentjiang <wentjiang@sina.com>
2024-12-17 19:12:33 +0800

    add question 203

---
+++ src/main/java/com/wentjiang/leetcode/q201_300/Question203_1.java
@@ -0,0 +1,20 @@
+package com.wentjiang.leetcode.q201_300;
+
+import com.wentjiang.leetcode.utils.ListNode;
+
+public class Question203_1 {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode preHead = new ListNode(-1);
+        preHead.next = head;
+        ListNode currentNode = preHead;
+        while (currentNode.next != null) {
+            ListNode nextNode = currentNode.next;
+            if (nextNode.val == val){
+                currentNode.next = nextNode.next;
+            }else{
+                currentNode = currentNode.next;
+            }
+        }
+        return preHead.next;
+    }
+}
