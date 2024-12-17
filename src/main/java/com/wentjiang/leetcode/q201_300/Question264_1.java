b157cda4e857d4f92b4615bbab8f85484fded54e
wentjiang <wentjiang@sina.com>
2024-12-17 16:45:08 +0800

    add question 264

---
+++ src/main/java/com/wentjiang/leetcode/q201_300/Question264_1.java
@@ -0,0 +1,31 @@
+package com.wentjiang.leetcode.q201_300;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class Question264_1 {
+    /**
+     * 使用优先级队列, 穷举所有符合规则的丑数,直到访问到n
+     * @param n
+     * @return
+     */
+    public int nthUglyNumber(int n) {
+        Set<Long> set = new HashSet<>();
+        PriorityQueue<Long> queue = new PriorityQueue<>();
+        queue.add(1L);
+        set.add(1L);
+        int uglyNumber = 1;
+        int[] factors = new int[]{2,3,5};
+        for (int i = 0; i < n; i++) {
+            uglyNumber = queue.poll().intValue();
+            for (int factor: factors) {
+                long newValue = (long) factor * uglyNumber;
+                if(set.add(newValue)){
+                    queue.add(newValue);
+                }
+            }
+        }
+        return uglyNumber;
+    }
+}
---
+++ src/main/java/com/wentjiang/leetcode/q201_300/Question264_2.java
@@ -0,0 +1,33 @@
+package com.wentjiang.leetcode.q201_300;
+
+public class Question264_2 {
+    /**
+     * 使用动态规划的方式
+     *
+     * @param n
+     * @return
+     */
+    public int nthUglyNumber(int n) {
+        int p2 = 0, p3 = 0, p5 = 0;
+        int[] dps = new int[n];
+        dps[0] = 1;
+        for (int i = 0; i < n - 1; i++) {
+            int p2Value = dps[p2] * 2;
+            int p3Value = dps[p3] * 3;
+            int p5Value = dps[p5] * 5;
+            int minValue = Math.min(p2Value, Math.min(p3Value, p5Value));
+            if (p2Value == minValue) {
+                p2++;
+            }
+            if (p3Value == minValue) {
+                p3++;
+            }
+            if (p5Value == minValue) {
+                p5++;
+            }
+
+            dps[i + 1] = minValue;
+        }
+        return dps[n - 1];
+    }
+}
