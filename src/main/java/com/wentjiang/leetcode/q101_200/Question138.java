eabe22f75ac21b660402a569f8083c2c76b9f06d
wentjiang <wentjiang@sina.com>
2024-12-21 13:49:11 +0800

    add 138

---
+++ src/main/java/com/wentjiang/leetcode/q101_200/Question138.java
@@ -0,0 +1,17 @@
+package com.wentjiang.leetcode.q101_200;
+
+
+import com.wentjiang.leetcode.utils.Node;
+
+public class Question138 {
+
+    /**
+     * @param head
+     * @return
+     */
+    public Node copyRandomList(Node head) {
+
+        return head;
+    }
+
+}
