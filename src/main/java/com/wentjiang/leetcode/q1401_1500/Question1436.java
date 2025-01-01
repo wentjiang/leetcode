6a9df2f29aaba231b885d3524f75e80735ebfb82
wentjiang <wentjiang@sina.com>
2025-01-01 18:45:19 +0800

    question 1436

---
+++ src/main/java/com/wentjiang/leetcode/q1401_1500/Question1436.java
@@ -0,0 +1,20 @@
+package com.wentjiang.leetcode.q1401_1500;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Question1436 {
+    public String destCity(List<List<String>> paths) {
+        Set<String> start = new HashSet<>();
+        for (List<String> path : paths) {
+            start.add(path.getFirst());
+        }
+        for (List<String> path : paths){
+            if (!start.contains(path.getLast())){
+                return path.getLast();
+            }
+        }
+        return "";
+    }
+}
