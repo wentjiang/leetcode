622e70d2bdba85551ddcf8a8626a338512f74155
wentjiang <wentjiang@sina.com>
2025-02-06 16:52:41 +0800

    add question77

--- src/main/java/com/wentjiang/leetcode/q1_100/Question77.java
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question77.java
@@ -1,17 +1,31 @@
 package com.wentjiang.leetcode.q1_100;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author wentao.jiang
- * 
- * @date 2020/2/11 9:45 PM
- * 
- * @description
- */
 public class Question77 {
+    /**
+     * 使用排列穷举法
+     */
     public List<List<Integer>> combine(int n, int k) {
-        // todo
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        int count = (int) Math.pow(2, n);
+        int i = 0;
+        while (i < count) {
+            if (Integer.bitCount(i) == k) {
+                String s = Integer.toBinaryString(i);
+                String sb = "0".repeat(Math.max(0, n - s.length())) + s;
+                char[] chars = sb.toCharArray();
+                List<Integer> temp = new ArrayList<>();
+                for (int j = 0; j < chars.length; j++) {
+                    if (chars[j] == '1') {
+                        temp.add(j + 1);
+                    }
+                }
+                result.add(temp);
+            }
+            i++;
+        }
+        return result;
     }
 }
