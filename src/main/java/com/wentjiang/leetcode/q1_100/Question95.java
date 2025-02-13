1febfaf02ecd81102c34170af87e5fa92b77f095
wentjiang <wentjiang@sina.com>
2025-02-13 14:16:24 +0800

    question95

---
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question95.java
@@ -0,0 +1,35 @@
+package com.wentjiang.leetcode.q1_100;
+
+import com.wentjiang.leetcode.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question95 {
+    public List<TreeNode> generateTrees(int n) {
+        return generateTrees(1, n);
+    }
+
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> result = new ArrayList<>();
+
+        if (start > end) {
+            result.add(null);
+            return result;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> lefts = generateTrees(start, i - 1);
+            List<TreeNode> rights = generateTrees(i + 1, end);
+            for (TreeNode left : lefts) {
+                for (TreeNode right : rights) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    result.add(root);
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/wentjiang/leetcode/q1_100/Question95Test.java
@@ -0,0 +1,15 @@
+package com.wentjiang.leetcode.q1_100;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Question95Test {
+
+    private Question95 question95 = new Question95();
+
+    @Test
+    void generateTrees() {
+        question95.generateTrees(3);
+    }
+}
