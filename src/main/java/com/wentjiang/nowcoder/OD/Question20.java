5c1bb03653012f90a34c56c9ef3848759eee3cc8
wentjiang <wentjiang@sina.com>
2025-02-19 16:43:09 +0800

    add questions

---
+++ src/main/java/com/wentjiang/nowcoder/OD/Question20.java
@@ -0,0 +1,12 @@
+package com.wentjiang.nowcoder.OD;
+
+public class Question20 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int getResult(int r, int[] arrays, int data){
+        double totalCapacity = Math.pow(2,r);
+
+    }
+}
