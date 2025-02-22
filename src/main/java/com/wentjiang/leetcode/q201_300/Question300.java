c6328eae9353a22f1e9f4a2dd2bd59804d4867b1
wentjiang <wentjiang@sina.com>
2025-02-22 21:40:16 +0800

    add question 300

---
+++ src/main/java/com/wentjiang/leetcode/q201_300/Question300.java
@@ -0,0 +1,18 @@
+package com.wentjiang.leetcode.q201_300;
+
+public class Question300 {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(dp[i], result);
+        }
+        return result;
+    }
+}
