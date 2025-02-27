af0c47761e8ca77da180b4f9d9d8e8c6de27bd99
wentjiang <wentjiang@sina.com>
2025-02-27 21:00:25 +0800

    add Question639

--- src/main/java/com/wentjiang/leetcode/q601_700/Question639.java
+++ src/main/java/com/wentjiang/leetcode/q601_700/Question639.java
@@ -1,10 +1,10 @@
 package com.wentjiang.leetcode.q601_700;
 
 public class Question639 {
-    //todo
+    static final int MOD = 1000000007;
     public int numDecodings(String s) {
         int n = s.length();
-        int[] dp = new int[n + 1];
+        long[] dp = new long[n + 1];
         dp[0] = 1;
 
         for (int i = 1; i <= n; i++) {
@@ -20,23 +20,29 @@ public class Question639 {
                 }
             }
 
-            //当前这一位和前一位的情况, 分为 **, 数*, *数, 数数
+            //第一位不能组合, 当前这一位和前一位的情况, 分为 **, 数*, *数, 数数
             if (i > 1) {
                 //**
                 if (s.charAt(i - 2) == '*' && s.charAt(i - 1) == '*') {
+                    // 11~19 21~26
                     dp[i] += dp[i - 2] * ((19 - 10) + (26 - 20));
                     //数*
+                    // 如果数是 1的情况 有11~19
                 } else if (s.charAt(i - 2) == '1' && s.charAt(i - 1) == '*') {
                     dp[i] += dp[i - 2] * (19 - 10);
                 } else if (s.charAt(i - 2) == '2' && s.charAt(i - 1) == '*') {
+                    //如果数是 2的情况 有21~26
                     dp[i] += dp[i - 2] * (26 - 20);
                     //*数
                 } else if (s.charAt(i - 2) == '*' && (s.charAt(i - 1) >= '0' && s.charAt(i - 1) <= '6')) {
-                    dp[i] += dp[i - 2] + 1;
+                    //如果是 *, 并且第二位的数字式0~6, 则生成的总数为之前的数量 X2
+                    dp[i] += dp[i - 2] * 2;
                 } else if (s.charAt(i - 2) == '*' && (s.charAt(i - 1) >= '7' && s.charAt(i - 1) <= '9')) {
+                    //如果是 7~9, 只有*=1时 才有组合,只有一种情况
                     dp[i] += dp[i - 2];
                     //数数
                 } else if (s.charAt(i - 2) != '*' && s.charAt(i - 1) != '*') {
+                    // 两个数的情况,只有第一位不为0 且生成的结果<=26才有效
                     if (s.charAt(i - 2) != '0') {
                         if (Integer.parseInt(String.valueOf(s.charAt(i - 2)) + String.valueOf(s.charAt(i - 1))) <= 26) {
                             dp[i] += dp[i - 2];
@@ -44,8 +50,9 @@ public class Question639 {
                     }
                 }
             }
+            dp[i] = dp[i] % MOD;
         }
 
-        return dp[n] % (int) (Math.pow(10, 9) + 7);
+        return (int) (dp[n] % MOD);
     }
 }
--- src/test/java/com/wentjiang/leetcode/q601_700/Question639Test.java
+++ src/test/java/com/wentjiang/leetcode/q601_700/Question639Test.java
@@ -38,4 +38,15 @@ class Question639Test {
     void numDecodings5() {
         Assertions.assertEquals(18720, question639.numDecodings("**1**"));
     }
+
+    @Test
+    void numDecodings6() {
+        Assertions.assertEquals(196465252, question639.numDecodings("7*9*3*6*3*0*5*4*9*7*3*7*1*8*3*2*0*0*6*"));
+    }
+
+
+    @Test
+    void numDecodings7() {
+        Assertions.assertEquals(882201566, question639.numDecodings("1*6*7*1*9*6*2*9*2*3*3*6*3*2*2*4*7*2*9*6*0*6*4*4*1*6*9*0*5*9*2*5*7*7*0*6*9*7*1*5*5*9*3*0*4*9*2*6*2*5*7*6*1*9*4*5*8*4*7*4*2*7*1*2*1*9*1*3*0*6*"));
+    }
 }
