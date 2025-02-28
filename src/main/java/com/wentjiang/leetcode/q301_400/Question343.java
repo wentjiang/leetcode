f583b680829687ec6cd9ac131a4706d8ed39dcbe
wentjiang <wentjiang@sina.com>
2025-02-28 20:01:20 +0800

    add question343

--- src/main/java/com/wentjiang/leetcode/q301_400/Question343.java
+++ src/main/java/com/wentjiang/leetcode/q301_400/Question343.java
@@ -11,14 +11,10 @@ public class Question343 {
         int[] dp = new int[n + 1];
         dp[1] = 1;
         dp[2] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = Integer.MIN_VALUE;
-            for (int j = i-1; j > 0; j--) {
-                if (i-j > dp[i-j]){
-                    dp[i] = Math.max(dp[i], (i - j) * j);
-                }else{
-                    dp[i] = Math.max(dp[i], dp[i-j] * j);
-                }
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i ; j++) {
+                int max = Math.max(j * (i - j), dp[j] * (i-j));
+                dp[i] = Math.max(max,dp[i]);
             }
         }
         return dp[n];
