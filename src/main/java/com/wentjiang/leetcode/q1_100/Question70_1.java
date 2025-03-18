f4a907c7f6db4f4a34a772f474cdcb973359f5bd
wentjiang <wentjiang@sina.com>
2025-03-18 15:05:53 +0800

    add question70

---
+++ src/main/java/com/wentjiang/leetcode/q1_100/Question70_1.java
@@ -0,0 +1,13 @@
+package com.wentjiang.leetcode.q1_100;
+
+public class Question70_1 {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
+}
